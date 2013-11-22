1ff087c93b7330f91c3730902029d21b7a868fe5
Pierre Neuvial <pierre.neuvial@genopole.cnrs.fr>
2013-11-22 16:34:16 +0000

    SuperLearner is now on CRAN...

--- inst/system/install.R
+++ inst/system/install.R
@@ -1,4 +1,5 @@
 ## for SuperLearning
+install.packages("SuperLearner")
 install.packages("gam")
 install.packages("glmnet")
 install.packages("nnls")
@@ -7,12 +8,3 @@ install.packages("polspline")
 install.packages("quadprog")
 install.packages("randomForest")
 install.packages("sgeostat")
-
-setwd("suggests")
-## install.packages("SuperLearner_1.1-18.tar.gz", repos=NULL) ## UPDATE from https://github.com/ecpolley/SuperLearner !!
-## git clone git://github.com/ecpolley/SuperLearner.git
-
-## To be retrieved from http://www.stat.berkeley.edu/users/laan/Software/DSA/
-install.packages("modelUtils_3.1.3.tar.gz")
-install.packages("DSA_3.1.3.tar.gz"
-setwd("..")
