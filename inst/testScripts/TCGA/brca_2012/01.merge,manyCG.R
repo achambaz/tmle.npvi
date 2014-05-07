70cc879027c62916115f533a9caf7759ca38d9e5
Pierre Neuvial <pierre.neuvial@genopole.cnrs.fr>
2014-05-07 12:45:26 +0000

    bidouilling data preprocessing scripts

--- inst/testScripts/TCGA/brca_2012/01.merge,manyCG.R
+++ inst/testScripts/TCGA/brca_2012/01.merge,manyCG.R
@@ -2,17 +2,34 @@ library(R.utils)
 path <- "data"
 path <- Arguments$getReadablePath(path)
 
+## - - - - - - - - - - - - - - - - - - - - - - -
+## 1. Methylation
+## - - - - - - - - - - - - - - - - - - - - - - -
+
+## 1a. data
 filename <- "BRCA.methylation.27k.450k.466.txt"
 pathname <- file.path(path, filename)
 
-mdat <- read.table(pathname, sep="\t", check.names=FALSE)
+mdat <- read.table(pathname, nr=2, sep="\t", check.names=FALSE)
+dim(mdat)
+## [1] 21986   466
+## one line per probe present in both 27k and 450k datasets (?)
+
 mpnames <- names(mdat)
 mpnames <- substr(mpnames, 1, 15)
 str(mpnames)
+
+rownames(mdat)
+
+mdat <- read.table(pathname, sep="\t", check.names=FALSE)
 names(mdat) <- mpnames
 methMat <- as.matrix(mdat)
 
-## annotation data (methylation)
+mpnames <- names(mdat)
+mpnames <- substr(mpnames, 1, 15)
+str(mpnames)
+
+## 1b. annotation
 apath <- "annotationData/chipTypes/HumanMethylation450"
 apath <- Arguments$getReadablePath(apath)
 afilename <- "HumanMethylation450_15017482_v.1.2.csv"
@@ -21,14 +38,84 @@ apathname <- file.path(apath, afilename)
 adat <- read.table(apathname, sep=",", skip=7, quote="\"", nr=485577, header=TRUE, as.is=TRUE)
 names(adat)
 
-## focus on probes that are on the microarray
+if (FALSE) { ## just in case
+  mgnames <- adat[["UCSC_RefGene_Name"]]
+  mgnames <- unique(unlist(strsplit(mgnames, ";")))
+
+  xfilename <- "HumanMethylation450,probeAnnotation,na36,PN20130301.xdr"
+  xpathname <- file.path(apath, xfilename)
+  dat <- loadObject(xpathname)
+  head(dat)
+}
+
+## focus on probes that are in the microarray
 cgNames <- rownames(mdat)  ## names of probes on the microarray
 mm <- match(cgNames, adat[["Name"]])
+(length(mm)==length(cgNames))  ## TRUE: all probes in 'mdat' are also in the annotation data file
+
 adat2 <- adat[mm, ]
 geneNames <- adat2[, "UCSC_RefGene_Name"]
+str(geneNames)
+## chr [1:21986] "ATP2A1;ATP2A1" "SLMAP" "MEOX2" "HOXD3" "PANX1" ...
 mgnames <- unique(unlist(strsplit(geneNames, ";")))
+str(mgnames)
+## chr [1:13956] "ATP2A1" "SLMAP" "MEOX2" "HOXD3" "PANX1" "COX8C" ...
 
-## painful effort 
+
+## - - - - - - - - - - - - - - - - - - - - - - -
+## 2. Copy number
+## - - - - - - - - - - - - - - - - - - - - - - -
+
+## data and annotation
+cpathname <- "data/GISTIC2/all_data_by_genes.txt"
+cdat <- read.table(cpathname, sep="\t", as.is=TRUE, header=TRUE, check.names=FALSE)
+
+cnMat <- as.matrix(cdat[, -c(1:3)])
+
+cpnames <- colnames(cnMat)
+## cpnames <- substr(cpnames, 1, 15)
+str(cpnames)
+
+cgnames <- cdat[["Gene Symbol"]]
+str(cgnames)
+
+## - - - - - - - - - - - - - - - - - - - - - - -
+## 2. Expression
+## - - - - - - - - - - - - - - - - - - - - - - -
+## expression gene and patient names
+epathname <- "data/BRCA.exp.466.med.txt"
+edat <- read.table(epathname, sep="\t", as.is=TRUE, header=TRUE, quote="\"", check.names=FALSE)
+egnames <- edat[["NAME"]]
+str(egnames)
+
+## data
+exprMat <- as.matrix(edat[, -1])
+colnames(exprMat) <- substr(colnames(exprMat), 1, 15)
+epnames <- colnames(exprMat)
+str(epnames)
+
+## gene names
+length(intersect(cgnames, mgnames))  ## 13261!
+length(intersect(cgnames, egnames))  ## 15466!
+length(intersect(intersect(cgnames, mgnames), egnames))  ## 11943
+
+gids <- intersect(intersect(cgnames, mgnames), egnames)  ## 15433!!
+
+eg <- match(gids, egnames)
+cg <- match(gids, cgnames)
+
+## patient names
+length(intersect(cpnames, mpnames)) 
+length(intersect(cpnames, epnames)) 
+length(intersect(intersect(cpnames, mpnames), epnames))
+
+pids <- intersect(intersect(cpnames, mpnames), epnames)
+
+ep <- match(pids, epnames)
+cp <- match(pids, cpnames)
+mp <- match(pids, mpnames)
+
+## methylation: mapping 'cg' ids to gene names
 x <- strsplit(geneNames, ";")
 x <- lapply(x, unique)
 xx <- sapply(x, paste, collapse=";")
@@ -49,67 +136,21 @@ if (FALSE) { ## sanity check
   unique(unlist(x[ids]))==gids[1]
 
   lens <- sapply(y$char, FUN=function(x) length(unlist(strsplit(x, " "))))
-  hist(lens)
+  table(lens)
 }
 
-## copy number gene names
-cpathname <- "data/GISTIC2/all_data_by_genes.txt"
-cdat <- read.table(cpathname, sep="\t", as.is=TRUE, header=TRUE, check.names=FALSE)
-cnMat <- as.matrix(cdat[, -c(1:3)])
-
-cpnames <- colnames(cnMat)
-## cpnames <- substr(cpnames, 1, 15)
-str(cpnames)
-
-cgnames <- cdat[["Gene Symbol"]]
-str(cgnames)
-
-## expression gene and patient names
-epathname <- "data/BRCA.exp.466.med.txt"
-edat <- read.table(epathname, sep="\t", as.is=TRUE, header=TRUE, quote="\"", check.names=FALSE)
-egnames <- edat[["NAME"]]
-str(egnames)
-
-exprMat <- as.matrix(edat[, -1])
-colnames(exprMat) <- substr(colnames(exprMat), 1, 15)
-epnames <- colnames(exprMat)
-str(epnames)
-
-## gene names
-length(intersect(cgnames, mgnames))  ## 13261!
-length(intersect(cgnames, egnames))  ## 15466!
-length(intersect(intersect(cgnames, mgnames), egnames))  ## 11943
-
-gids <- intersect(intersect(cgnames, mgnames), egnames)
-
-eg <- match(gids, egnames)
-cg <- match(gids, cgnames)
-
-## patient names
-length(intersect(cpnames, mpnames)) 
-length(intersect(cpnames, epnames)) 
-length(intersect(intersect(cpnames, mpnames), epnames))
-
-pids <- intersect(intersect(cpnames, mpnames), epnames)
-
-ep <- match(pids, epnames)
-cp <- match(pids, cpnames)
-mp <- match(pids, mpnames)
-
-## export
+## - - - - - - - - - - - - - - - - - - - - - - -
+## 4. Data export
+## - - - - - - - - - - - - - - - - - - - - - - -
 gid <- gids[1]
 
 ## gene expression
 idxE <- eg[match(gid, gids)]
 geneExpr <- exprMat[idxE, ep]
-str(geneExpr)
 
 ## DNA copy number
 idxC <- cg[match(gid, gids)]
 copyNumber <- cnMat[idxC, cp]
-str(copyNumber)
-
-stopifnot(identical(names(copyNumber), names(geneExpr)))
 
 ## methylation
 idxM <- mg[match(gid, gids)]
@@ -123,8 +164,8 @@ rownames(methyl) <- paste("W", 1:nrow(methyl), sep="")
 obs <- cbind(Y=geneExpr, X=copyNumber, W=t(methyl))
 str(obs)
 
-pairs(obs)
+## pairs(obs)
 
+saveObject(obs, file="obs,ATAD3A,2.xdr")
 
-saveObject(obs, file="obs,ATAD3A.xdr")
 
