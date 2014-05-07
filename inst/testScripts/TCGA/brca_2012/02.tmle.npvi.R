f1336e4c2e4cf0761442e6dd6de1afd1a2cb490f
Antoine Chambaz <achambaz@u-paris10.fr>
2014-05-07 14:50:32 +0000

    adding 02.tmle.npvi.R

---
+++ inst/testScripts/TCGA/brca_2012/02.tmle.npvi.R
@@ -0,0 +1,25 @@
+library(tmle.npvi)
+library("R.utils")
+log <- Arguments$getVerbose(-8, timestamp=TRUE)
+
+path <- "geneData/tcga_brca_2012"
+path <- Arguments$getReadablePath(path)
+files <- list.files(path)
+
+TMLE <- vector("list", length(files))
+for (ii in 1:2) {# length(files)) {
+  ## loading the data
+  pathname <- file.path(path, files[ii])
+  obs <- loadObject(pathname)
+  ## thresholding copy number data
+  whichSmall <- which(abs(obs[, "X"]) <= 2e-2)
+  obs[whichSmall, "X"] <- 0
+  ##
+  tmle <- try(tmle.npvi(obs, f=identity, flavor="superLearning"))
+  if (inherits(tmle, "try-error")) {
+    TMLE[[ii]] <- NA
+  } else {
+    TMLE[[ii]] <- tmle
+  }
+}
+names(TMLE) <- unlist(strsplit(files, split=".xdr"))
