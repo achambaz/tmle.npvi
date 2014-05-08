5cbe99bb26445e84d641e381e51fc72c07d2e4d4
Antoine Chambaz <achambaz@u-paris10.fr>
2014-05-08 07:12:56 +0000

    new version of 02.tmle.npvi.R

--- inst/testScripts/TCGA/brca_2012/02.tmle.npvi.R
+++ inst/testScripts/TCGA/brca_2012/02.tmle.npvi.R
@@ -6,21 +6,51 @@ path <- "geneData/tcga_brca_2012"
 path <- Arguments$getReadablePath(path)
 files <- list.files(path)
 
+where <- unlist(strsplit(Sys.info()["nodename"], split="\\."))[1]
+if (where=="grizzlybear") {
+  cArgs <- commandArgs()
+  chunk <- as.character(cArgs[5])
+  idx <- eval(parse(text=sub("-", ":", chunk)))
+} else {
+  idx <- 1:length(files)
+  chunk <- paste(as.character(idx[c(1, length(idx))]), collapse="-")
+}
+
+descr <- list(thresh=2e-2,
+              f=identity,
+              flavor="learning",
+              iter=10,
+              stoppingCriteria=list(mic = 0.001, div = 0.001, psi = 0.01))
+
+fileout <- paste(descr$flavor, "chromosome21", chunk, "RData", sep=".")
+
 TMLE <- vector("list", length(files))
 names(TMLE) <- unlist(strsplit(files, split=".xdr"))
-for (ii in 1:1) {# length(files)) {
+
+counter <- 0
+for (ii in idx) {
+  counter <- counter+1
   ## loading the data
   pathname <- file.path(path, files[ii])
   obs <- loadObject(pathname)
+  nbcov <- ncol(extractW(obs))
   ## thresholding copy number data
-  whichSmall <- which(abs(obs[, "X"]) <= 2e-2)
+  whichSmall <- which(abs(obs[, "X"]) <= descr$thresh)
   obs[whichSmall, "X"] <- 0
   ##
-  tmle <- try(tmle.npvi(obs, f=identity, flavor="learning"))
+  tmle <- try(tmle.npvi(obs, f=descr$f, flavor=descr$flavor,
+                        stoppingCriteria=descr$stoppingCriteria))
   if (inherits(tmle, "try-error")) {
     TMLE[[ii]] <- NA
   } else {
-    TMLE[[ii]] <- tmle
+    TMLE[[ii]] <- list(nbcov=nbcov,
+                       hist=getHistory(tmle))
+  }
+  ## saving
+  if (counter==10) {
+    save(descr, TMLE, file=fileout)
+    counter <- 0
   }
 }
 
+save(descr, TMLE, file=fileout)
