0e328ba2d8a3c4589aac2946a7389fd44e69a25b
Antoine Chambaz <achambaz@u-paris10.fr>
2014-09-06 06:15:30 +0000

    SL ameliore aussi

--- data/superLearningLib.R
+++ data/superLearningLib.R
@@ -63,9 +63,17 @@ SL.glm.condExpX2givenW <- function#SL  Wrapper for Estimation of Cond. Expect. o
 (Y, X, newX, family, obsWeights, ...) {
   ##seealso<< learnCondExpX2givenW, predict.SL.glm.condExpX2givenW
   varNames <- names(X)
+  if (length(varNames)>20) {
+    varNames <- varNames[1:20]
+    warning(paste("Using only", paste(varNames, collapse=", "), "in 'SL.glm.condExpX2givenW'"))
+  }
   theFormula <- paste(varNames, collapse=" + ")
-  theFormula2 <- paste("I(", varNames, "^2)", collapse=" + ", sep="")
-  theFormula <- paste("Y ~", theFormula, "+", theFormula2, sep=" ")
+  if (length(varNames)<=10) {
+    theFormula2 <- paste("I(", varNames, "^2)", collapse=" + ", sep="")
+    theFormula <- paste("Y ~", theFormula, "+", theFormula2, sep="")
+  } else {
+    theFormula <- paste("Y ~", theFormula, sep="")
+  } 
   ## formula.glm.condExpX2givenW <- as.formula(Y~W+I(W^2));
   formula.glm.condExpX2givenW <- as.formula(theFormula);
   
@@ -100,9 +108,17 @@ SL.glm.condExpXYgivenW <- function#SL  Wrapper for Estimation of Cond. Expect. o
 (Y, X, newX, family, obsWeights, ...) {
   ##seealso<< learnCondExpXYgivenW, predict.SL.glm.condExpXYgivenW
   varNames <- names(X)
+  if (length(varNames)>20) {
+    varNames <- varNames[1:20]
+    warning(paste("Using only", paste(varNames, collapse=", "), "in 'SL.glm.condExpXYgivenW'", collapse=""))
+  }
   theFormula <- paste(varNames, collapse=" + ")
-  theFormula2 <- paste("I(", varNames, "^2)", collapse=" + ", sep="")
-  theFormula <- paste("Y ~", theFormula, "+", theFormula2, sep=" ")
+  if (length(varNames)<=10) {
+    theFormula2 <- paste("I(", varNames, "^2)", collapse=" + ", sep="")
+    theFormula <- paste("Y ~", theFormula, "+", theFormula2, sep="")
+  } else {
+    theFormula <- paste("Y ~", theFormula, sep="")
+  }
   ## formula.glm.condExpXYgivenW <- as.formula(Y~W+I(W^2));
   formula.glm.condExpXYgivenW <- as.formula(theFormula);
   
@@ -136,11 +152,22 @@ SL.glm.g <- function#SL  Wrapper for Estimation of Cond. Prob. of X=0 Given W
 (Y, X, newX, family, obsWeights, ...) {
   ##seealso<< learnG, predict.SL.glm.g
   varNames <- names(X)
+  if (length(varNames)>20) {
+    varNames <- varNames[1:20]
+    warning(paste("Using only", paste(varNames, collapse=", "), "in 'SL.glm.g'"))
+  }
   theFormula <- paste(varNames, collapse=" + ")
-  theFormula2 <- paste("I(", varNames, "^2)", collapse=" + ", sep="")
-  theFormula <- paste("Y ~", theFormula, "+", theFormula2, sep=" ")
+  if (length(varNames)<=10) {
+    theFormula2 <- paste("I(", varNames, "^2)", collapse=" + ", sep="")
+    theFormula <- paste("Y ~", theFormula, "+", theFormula2, sep=" ")
+  } else {
+    theFormula <- paste("Y ~", theFormula, sep="")
+  }
   ## formula.glm.g <- as.formula(Y~W+I(W^2));
   formula.glm.g <- as.formula(theFormula);
+
+
+
   fit.glm <- glm(formula.glm.g, data = X, family = family,
                  weights = obsWeights)
   pred <- predict(fit.glm, newdata = newX, type = "response")
@@ -171,11 +198,20 @@ SL.glm.theta <- function#SL  Wrapper for Estimation of Cond. Prob. of X=0 Given 
 (Y, X, newX, family, obsWeights, ...) {
   ##seealso<< learnTheta, predict.SL.glm.theta
   varNames <- names(X)
-  theFormula <- paste(varNames, collapse="*")
-  theFormula <- paste("Y ~", theFormula, sep=" ")
+  if (length(varNames)>20) {
+    varNames <- varNames[1:20]
+    warning(paste("Using only 'X' and", paste(varNames, collapse=", "), "in 'SL.glm.theta'"))
+  }
+  theFormula <- paste(varNames, collapse=" + ")
+  if (length(varNames)<=10) {
+    theFormula <- paste("Y ~ X+", theFormula, "+ X*(",
+                        theFormula, ")", sep="")
+  } else {
+    theFormula <- paste("Y ~ X+", theFormula, sep="")
+  }
   ## formula.glm.theta <- as.formula(Y~X*W);
   formula.glm.theta <- as.formula(theFormula);
-  
+
   fit.glm <- glm(formula.glm.theta, data = X, family = family, 
                  weights = obsWeights)
   pred <- predict(fit.glm, newdata = newX, type = "response")
