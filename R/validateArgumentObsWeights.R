2fed051c9be1f300768b6c3d1dd2280fc5f354d7
Antoine Chambaz <achambaz@u-paris10.fr>
2015-10-02 10:10:39 +0000

    version avec poids

--- R/validateArgumentObsWeights.R
+++ R/validateArgumentObsWeights.R
@@ -9,7 +9,7 @@ validateArgumentObsWeights <- function(weights, nobs) {
     if (length(weights)!=nobs) {
       throw("Argument 'weights' should be a vector of size ", nobs, ", not ", length(weights));
     }
-    sumWeights <- sum(weights)
+    sumWeights <- zapsmall(sum(weights))
     if (sumWeights!=1) {
       throw("Argument 'weights' should be a vector of non-negative numbers summing up to 1, not ", sumWeights); 
     }
