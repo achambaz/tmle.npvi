38e0ea35d1ba0ca9adfa30374812176c2e4b5927
Pierre Neuvial <pierre.neuvial@genopole.cnrs.fr>
2015-10-13 14:03:32 +0000

    Added tests for the implementation of cluster ids

--- inst/testScripts/00.testingWeighting.R
+++ inst/testScripts/00.testingWeighting.R
@@ -60,3 +60,37 @@ obsC[, "X"] <- obsC[, "X"] - X0
 obs3 <- obsC
 
 npvi3 <- tmle.npvi(obs3, f=identity, flavor="learning", B=5e4, nMax=10)
+
+## with clusters
+sim <- getSample(sampleSize, O, lambda0, p=p, omega=omega, sigma2=1, Sigma3=S)
+obs <- sim$obs
+V <- matrix(runif(3*nrow(obs)), ncol=3)
+colnames(V) <- paste("V", 1:3, sep="")
+obs <- cbind(V, obs)
+X0 <- O[2,2]
+obsC <- obs
+obsC[, "X"] <- obsC[, "X"] - X0
+obs <- obsC
+
+npvi.noclust <- tmle.npvi(obs, f=identity, flavor="learning", B=5e4, nMax=10)
+
+K <- 10
+npvi.clust <- tmle.npvi(obs, f=identity, flavor="learning", B=5e4, nMax=10, id=rep(1:K, each=nrow(obs)/K))
+summary(npvi.clust$.obsWeights)
+
+Ks <- c(2, 4, 5, 10, 20, 25, 50, 100, 1000, 10000)
+ncores <- 4
+library("parallel")
+res <- mclapply(Ks, mc.cores=ncores, FUN=function(K) {
+           npvi.clust <- tmle.npvi(obs, f=identity, flavor="learning", B=5e4, nMax=10, id=rep(1:K, each=nrow(obs)/K))
+           tmle.npvi:::getSic.NPVI(npvi.clust)
+       }
+)
+## scaling factor between standard error estimate without clusters and with clusters:
+fact <- tmle.npvi:::getSic.NPVI(npvi.noclust)/unlist(res)
+
+plot(Ks,  fact, log=c("x", "y"))
+lines(Ks,  sqrt(nrow(obs)/Ks), pch=20, col=2, t="b")
+## works as expected as soon as the number of clusters is large enough
+
+
